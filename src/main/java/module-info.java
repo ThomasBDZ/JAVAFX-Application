c727d5a76a96b2add4526a72536d786a30384ba9
BALDUZ Thomas <thomas.balduz@telecomnancy.eu>
2022-01-17 21:41:48 +0100

    fix some pb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module eu.telecomnancy.javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.desktop;
-    opens eu.telecomnancy.javafx to javafx.fxml;
+    requires java.sql;
     opens eu.telecomnancy.javafx.controller to javafx.fxml;
 
     exports eu.telecomnancy.javafx;
+    exports eu.telecomnancy.javafx.controller;
 }
