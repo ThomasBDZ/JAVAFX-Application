3d61a76224662987a46a76711de7ce4d9b4e7d9e
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-17 16:02:52 +0100

    [ADD] Main

--- src/main/java/eu/telecomnancy/javafx/MainApp.java
+++ src/main/java/eu/telecomnancy/javafx/MainApp.java
@@ -1,5 +1,7 @@
 package eu.telecomnancy.javafx;
 
+import eu.telecomnancy.javafx.controller.Connexion;
+import eu.telecomnancy.javafx.modèle.ProfRDV;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,17 +11,33 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+    // @Override
+    // public void start(Stage stage) throws Exception {
+    //     Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene.fxml"));
+
+    //     Scene scene = new Scene(root);
+    //     scene.getStylesheets().add(getClass().getResource("/stylesheet/style.css").toExternalForm());
+
+    //     stage.setTitle("JavaFX and Gradle");
+    //     stage.setScene(scene);
+    //     stage.show();
+    // }
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene.fxml"));
+    public void start(Stage primaryStage) throws Exception {
 
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/stylesheet/style.css").toExternalForm());
-
-        stage.setTitle("JavaFX and Gradle");
-        stage.setScene(scene);
-        stage.show();
+        ProfRDV profRDV = new ProfRDV();
+        FXMLLoader loader = new FXMLLoader(); 
+        loader.setLocation(getClass().getResource("/fxml/Principale.fxml"));
+        Connexion connexionController = new Connexion(profRDV);
+        loader.setControllerFactory(ic -> {
+        if (ic.equals(eu.telecomnancy.javafx.controller.Connexion.class)) return connexionController;
+        else return null ;
+        });
+        Parent root = loader.load() ;
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.show();
     }
+    
 
     public static void main(String[] args) {
         launch(args);
