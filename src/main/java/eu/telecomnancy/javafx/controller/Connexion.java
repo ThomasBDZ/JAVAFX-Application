492a1e3e800cc699a3761d3992b0c3885f0d3d55
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-17 16:49:49 +0100

    [UPD] Controlers

--- src/main/java/eu/telecomnancy/javafx/controller/Connexion.java
+++ src/main/java/eu/telecomnancy/javafx/controller/Connexion.java
@@ -1,5 +1,6 @@
 package eu.telecomnancy.javafx.controller;
 
+import eu.telecomnancy.javafx.controller.Erreurs.ConnexionError;
 import eu.telecomnancy.javafx.model.ProfRDV;
     /**
      * Controleur de la vue Connexion.fxml
@@ -13,6 +14,18 @@ public class Connexion {
         this.profRDV = profRDV;
     }
 
+    /**
+     * Méthode pour valider la connexion issue de la vue Connexion.fxml.
+     * Gère les erreurs qui peuvent être remontée (MailInexistant et MauvaisMdp)
+     */
+    public void validationConnexion(){
+
+        // try {
+            
+        // } catch (ConnexionError e) {
+        //     //TODO: handle exception
+        // }
+    }
     
 
 
--- src/main/java/eu/telecomnancy/javafx/controller/MenuBarPrincipal.java
+++ src/main/java/eu/telecomnancy/javafx/controller/MenuBarPrincipal.java
@@ -14,6 +14,33 @@ public class MenuBarPrincipal {
         this.profRDV = profRDV;
     }
    
+    /**
+     * Controle du bouton de deconnexion dans le menu bar principal.
+     * Si l'utilisateur est connecté.
+     * Non affiché si on est pas connecté.
+     */
+    public void deconnexion(){
+
+    }
+
+    /**
+     * Controle du bouton accueil dans le menu bar principale. 
+     * --> Renvoit vers la page d'accueil si connecté.
+     * --> Ne fait rien sinon.
+     */
+    public void accueil(){
+
+    }
+
+    /**
+     * Controle du bouton quitter dans le menu bar principale. 
+     * --> Quitte la page.
+     */
+    public void quitter(){
+        //TODO: ajout de la demande de confirmation avant de quitter
+
+        
+    }
     
     
 }
