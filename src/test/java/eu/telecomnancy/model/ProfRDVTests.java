f61c863098d505a7d87f332d16a8e3bc0b7afc65
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-18 00:37:05 +0100

    [ADD] structures pour tests

---
+++ src/test/java/eu/telecomnancy/model/ProfRDVTests.java
@@ -0,0 +1,17 @@
+package eu.telecomnancy.javafx.Tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ProfRDVTests {
+
+    // @BeforeAll
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+    
+    
+}
