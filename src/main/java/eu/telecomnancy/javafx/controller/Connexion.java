0fe4f702b9d5131d798b93a1c1e9ebd602d68e32
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-17 15:45:57 +0100

    [ADD] Ajout des controlleurs

---
+++ src/main/java/eu/telecomnancy/javafx/controller/Connexion.java
@@ -0,0 +1,20 @@
+package eu.telecomnancy.javafx.controller;
+
+import eu.telecomnancy.javafx.modèle.ProfRDV;
+
+public class Connexion {
+
+    ProfRDV profRDV;
+    /**
+     * Controleur de la vue Connexion.fxml
+     */
+   
+    public Connexion(ProfRDV profRDV){
+        this.profRDV = profRDV;
+    }
+
+    
+
+
+    
+}
---
+++ src/main/java/eu/telecomnancy/javafx/controller/MenuBarPrincipal.java
@@ -0,0 +1,18 @@
+package eu.telecomnancy.javafx.controller;
+
+import eu.telecomnancy.javafx.modèle.ProfRDV;
+
+public class MenuBarPrincipal {
+    /**
+     * Controleur de la vue MenuBar.fxml
+     */
+
+    ProfRDV profRDV;
+
+    public MenuBarPrincipal(ProfRDV profRDV){
+        this.profRDV = profRDV;
+    }
+   
+    
+    
+}
