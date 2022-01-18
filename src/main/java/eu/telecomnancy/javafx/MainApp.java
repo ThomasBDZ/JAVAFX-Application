571951edc7fc1122dcb8c78cc5e04dfc50ecc273
Gisel Rodriguez <gisel.rodriguez-baide@telecomnancy.eu>
2022-01-18 16:51:19 +0100

    add DescriptionRDV.fxml

--- src/main/java/eu/telecomnancy/javafx/MainApp.java
+++ src/main/java/eu/telecomnancy/javafx/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
 
         ProfRDV profRDV = new ProfRDV();
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/fxml/RecapRDV.fxml"));
+        loader.setLocation(getClass().getResource("/fxml/DescriptionRDV.fxml"));
         Connexion connexionController = new Connexion(profRDV);
         MenuBarPrincipal menuBarPrincipal = new MenuBarPrincipal(profRDV);
         loader.setControllerFactory(ic -> {
