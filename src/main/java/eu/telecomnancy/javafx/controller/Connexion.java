7c4ed61b8bcd146dee210f25354abb09430292c3
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-18 11:41:44 +0100

    [ADD] Tmp changement fxml

--- src/main/java/eu/telecomnancy/javafx/controller/Connexion.java
+++ src/main/java/eu/telecomnancy/javafx/controller/Connexion.java
@@ -3,10 +3,18 @@ package eu.telecomnancy.javafx.controller;
 import eu.telecomnancy.javafx.controller.Erreurs.ConnexionError;
 import eu.telecomnancy.javafx.model.GestionnaireDB;
 import eu.telecomnancy.javafx.model.ProfRDV;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
     /**
      * Controleur de la vue Connexion.fxml
      */
@@ -16,6 +24,9 @@ public class Connexion {
     private ProfRDV profRDV;
 
     @FXML
+    private VBox vboxConnexion;
+
+    @FXML
     private MenuItem menuItemAccueil;
 
     @FXML
@@ -23,6 +34,9 @@ public class Connexion {
 
     @FXML
     private PasswordField mdp;
+
+    @FXML
+    private Button connexion;
 
 
     public Connexion(ProfRDV profRDV){
@@ -34,15 +48,34 @@ public class Connexion {
      * Gère les erreurs qui peuvent être remontée (MailInexistant et MauvaisMdp)
      */
     public void validationConnexion(){
-
-        GestionnaireDB gestionnaireDB = new GestionnaireDB();
-        gestionnaireDB.login(mdp,id);
-        
         // try {
+
+        //     GestionnaireDB gestionnaireDB = new GestionnaireDB();
+        //     gestionnaireDB.login(mdp,id);
             
-        // } catch (ConnexionError e) {
+        // } catch (Error e){
+
+        // }
+        // catch (ConnexionError e) {
+        //      Label erreur = e.getError();
+        //      vboxConnexion.getChildren().add(erreur);
         //     //TODO: handle exception
         // }
+
+        Node node=(Node) connexion;
+        Stage stage =(Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(); 
+        loader.setLocation(getClass().getResource("/fxml/Calendrier.fxml"));
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1000, 1000);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+
     }
     
 }
