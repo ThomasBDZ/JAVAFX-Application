470472e34778ce1a95b2b60036cf55131407c57e
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-19 15:26:24 +0100

    [ADD] VariablesGlobales

---
+++ src/main/java/eu/telecomnancy/javafx/model/utils/GlobalVariables.java
@@ -0,0 +1,10 @@
+package eu.telecomnancy.javafx.model.utils;
+
+public class GlobalVariables {
+
+
+    public final static int HEURE_MIN = 8; // 8H
+    public final static int HEURE_MAX = 20; // 20H
+    public final static int INDICE_MAX = HEURE_MAX*3 ; 
+    
+}
