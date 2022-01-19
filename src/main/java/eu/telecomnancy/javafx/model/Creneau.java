12e884fa971b344d539072b99a3b56c8c1c57ff1
Gisel Rodriguez <gisel.rodriguez-baide@telecomnancy.eu>
2022-01-19 15:49:18 +0100

    change classe Creneau

--- src/main/java/eu/telecomnancy/javafx/model/Creneau.java
+++ src/main/java/eu/telecomnancy/javafx/model/Creneau.java
@@ -1,14 +1,29 @@
 package eu.telecomnancy.javafx.model;
 
+import eu.telecomnancy.javafx.model.utils.GlobalVariables;
+
 import java.time.LocalTime;
 import java.util.Date;
 
+import static eu.telecomnancy.javafx.model.utils.GlobalVariables.*;
+
+
 public class Creneau {
-    public Boolean disponible;
     public int indice;
-    public LocalTime heure;
     public Date date;
 
+
+    public Creneau(int indice, Date date ){
+        this.indice=indice;
+        this.date=date;
+
+    }
+
+    String getHeure(){
+        int heure= indice/3+HEURE_MIN;
+        int minutes=(indice)%3*20;
+        return heure+"h"+minutes;
+    }
 
 
 }
