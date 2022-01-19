5d0c26f54c0a7ed06f5c676166e19e2fae487316
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-19 12:11:27 +0100

    [UPD] AccesPage - non finis

---
+++ src/main/java/eu/telecomnancy/javafx/controller/AccesPages.java
@@ -0,0 +1,137 @@
+package eu.telecomnancy.javafx.controller;
+
+
+import eu.telecomnancy.javafx.model.ProfRDV;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+public class AccesPages {
+
+    private ProfRDV profRDV;
+
+    public AccesPages(ProfRDV profRDV){
+        this.profRDV = profRDV;
+    }
+    
+    public void accesAccueilConnexion(Node node){
+        Stage stage =(Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(); 
+        loader.setLocation(getClass().getResource("/fxml/Connexion.fxml"));
+        MenuBarPrincipal menuBarPrincipal = new MenuBarPrincipal(profRDV);
+        Connexion connexionController = new Connexion(profRDV);
+        loader.setControllerFactory(ic -> {
+        if (ic.equals(eu.telecomnancy.javafx.controller.Connexion.class)) return connexionController;
+        if (ic.equals(eu.telecomnancy.javafx.controller.MenuBarPrincipal.class)) return menuBarPrincipal;
+        else return null ;
+        });
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1000, 1000);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("exception chargement page AccueilConnexion - AccesAccueil");
+            //TODO: handle exception
+        }
+
+    }
+
+
+    public void accesAccueilEtudiant(Node node, String path){
+        Stage stage =(Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(); 
+        AccueilEnseignant accueilEnseignant = new AccueilEnseignant(profRDV);
+        AccueilEtudiant accueilEtudiant = new AccueilEtudiant(profRDV);
+        MenuBarPrincipal menuBarPrincipal = new MenuBarPrincipal(profRDV);
+        Connexion connexionController = new Connexion(profRDV);
+        Calendrier calendrier = new Calendrier(profRDV);
+        loader.setLocation(getClass().getResource(path));
+        loader.setControllerFactory(ic -> {
+        if (ic.equals(eu.telecomnancy.javafx.controller.Connexion.class)) return connexionController;
+        if (ic.equals(eu.telecomnancy.javafx.controller.MenuBarPrincipal.class)) return menuBarPrincipal;
+        if (ic.equals(eu.telecomnancy.javafx.controller.AccueilEtudiant.class)) return accueilEtudiant;
+        if (ic.equals(eu.telecomnancy.javafx.controller.AccueilEnseignant.class)) return accueilEnseignant;
+        if (ic.equals(eu.telecomnancy.javafx.controller.Calendrier.class)) return calendrier;
+        else return null ;
+        });
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1000, 1000);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("exception ---- ");
+            //TODO: handle exception
+        }
+
+    }
+
+    public void accesAccueilEnseignant(Node node, String path){
+        Stage stage =(Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(); 
+        AccueilEnseignant accueilEnseignant = new AccueilEnseignant(profRDV);
+        AccueilEtudiant accueilEtudiant = new AccueilEtudiant(profRDV);
+        MenuBarPrincipal menuBarPrincipal = new MenuBarPrincipal(profRDV);
+        Connexion connexionController = new Connexion(profRDV);
+        Calendrier calendrier = new Calendrier(profRDV);
+        loader.setLocation(getClass().getResource(path));
+        loader.setControllerFactory(ic -> {
+        if (ic.equals(eu.telecomnancy.javafx.controller.Connexion.class)) return connexionController;
+        if (ic.equals(eu.telecomnancy.javafx.controller.MenuBarPrincipal.class)) return menuBarPrincipal;
+        if (ic.equals(eu.telecomnancy.javafx.controller.AccueilEtudiant.class)) return accueilEtudiant;
+        if (ic.equals(eu.telecomnancy.javafx.controller.AccueilEnseignant.class)) return accueilEnseignant;
+        if (ic.equals(eu.telecomnancy.javafx.controller.Calendrier.class)) return calendrier;
+        else return null ;
+        });
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1000, 1000);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("exception ---- ");
+            //TODO: handle exception
+        }
+
+    }
+
+    public void accesAccueilAdmin(Node node, String path){
+        Stage stage =(Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(); 
+        AccueilEnseignant accueilEnseignant = new AccueilEnseignant(profRDV);
+        AccueilEtudiant accueilEtudiant = new AccueilEtudiant(profRDV);
+        MenuBarPrincipal menuBarPrincipal = new MenuBarPrincipal(profRDV);
+        Connexion connexionController = new Connexion(profRDV);
+        Calendrier calendrier = new Calendrier(profRDV);
+        loader.setLocation(getClass().getResource(path));
+        loader.setControllerFactory(ic -> {
+        if (ic.equals(eu.telecomnancy.javafx.controller.Connexion.class)) return connexionController;
+        if (ic.equals(eu.telecomnancy.javafx.controller.MenuBarPrincipal.class)) return menuBarPrincipal;
+        if (ic.equals(eu.telecomnancy.javafx.controller.AccueilEtudiant.class)) return accueilEtudiant;
+        if (ic.equals(eu.telecomnancy.javafx.controller.AccueilEnseignant.class)) return accueilEnseignant;
+        if (ic.equals(eu.telecomnancy.javafx.controller.Calendrier.class)) return calendrier;
+        else return null ;
+        });
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1000, 1000);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("exception ---- ");
+            //TODO: handle exception
+        }
+
+    }
+
+}
