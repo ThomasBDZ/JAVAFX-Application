f1cfdc90822f77ec1ef049d24d26de07a3422790
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-01-20 08:45:21 +0100

    [UPD] Merge des deux creneaux

--- src/main/java/eu/telecomnancy/javafx/model/Creneau.java
+++ src/main/java/eu/telecomnancy/javafx/model/Creneau.java
@@ -1,20 +1,53 @@
 package eu.telecomnancy.javafx.model;
 
-
 import java.util.Date;
 
-import static eu.telecomnancy.javafx.model.utils.GlobalVariables.*;
-
-
 public class Creneau {
-    public int indice;
-    public Date date;
 
+    public Creneau(){
 
-    public Creneau(int indice, Date date ){
+    }
+    private int id;
+    private int id_prof;
+    private int indice;
+    private java.util.Date date;
+
+    public Creneau(int indice, Date date){
         this.indice=indice;
         this.date=date;
 
+    }
+
+    public Creneau(int id, int id_prof, int indice, java.util.Date date){
+        this.date = date;
+        this.id = id;
+        this.id_prof = id_prof;
+        this.indice = indice;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setId_prof(int id_prof){
+        this.id_prof = id_prof;
+    }
+    public void setIndice(int indice){
+        this.indice = indice;
+    }
+    public void setDate(java.util.Date date){
+        this.date = date;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    public int getId_prof(){
+        return this.id_prof;
+    }
+    public int getIndice(){
+        return this.indice;
+    }
+    public java.util.Date getDate(){
+        return this.date;
     }
 
     String getHeure(){
