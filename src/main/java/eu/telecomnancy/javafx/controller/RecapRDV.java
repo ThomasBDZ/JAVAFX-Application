7afc078b9c9487446430e8c382a2e8a611b14bf4
Gisel Rodriguez <gisel.rodriguez-baide@telecomnancy.eu>
2022-01-21 11:39:54 +0100

    modif recapRDV

--- src/main/java/eu/telecomnancy/javafx/controller/RecapRDV.java
+++ src/main/java/eu/telecomnancy/javafx/controller/RecapRDV.java
@@ -1,10 +1,24 @@
 package eu.telecomnancy.javafx.controller;
 
-import eu.telecomnancy.javafx.model.ProfRDV;
-import eu.telecomnancy.javafx.model.RDV;
+import eu.telecomnancy.javafx.model.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class RecapRDV extends Controlleur{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RecapRDV extends Controlleur implements Initializable {
+
     private RDV rdv;
+
+    public Boolean status;
+    public Boolean archive;
+    public String lieu;
+    public String libelle;
+
     public RecapRDV(ProfRDV profRDV){
         super(profRDV);
     }
@@ -12,5 +26,32 @@ public class RecapRDV extends Controlleur{
     public RecapRDV(ProfRDV profRDV, RDV rdv) {
         super(profRDV);
         this.rdv=rdv;
+    }
+
+    @FXML
+    public Label labelDate;
+
+    @FXML
+    public Label labelHeure;
+
+    @FXML
+    public Label labelEnseignant;
+
+    @FXML
+    public TextField fieldLieu;
+
+    @FXML
+    public TextField  fieldDescription;
+
+    @FXML
+    public TextField  fieldLibelle;
+
+    @FXML
+    public Button valider;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
